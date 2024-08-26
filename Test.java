ec74b84837e5e7b1cb3832f07ee11e405f7a076b
Nithilan <r.nithilan@gmail.com>
2024-08-26 12:49:40 +0530

    1st Commit

---
+++ Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void main(String[] args){
+
+        Test operationTest = new Test();
+        System.out.println(operationTest.add(2,3));
+    }
+
+    public int add(int a, int b){
+        return a + b;
+    }
+}
